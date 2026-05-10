1b2f0dc490d3f09a0f2213c7cf1533b3c750eb42
IDRIS GHAMID <idris.ghamid@gmail.com>
2026-05-09 19:03:23 -0700

    Update source.config.ts

--- docs/idris-db-docs/source.config.ts
+++ docs/idris-db-docs/source.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
-import { remarkVersions } from './lib/remark-versions';
 import lastModified from 'fumadocs-mdx/plugins/last-modified';
 
 export const docs = defineDocs({
@@ -14,7 +13,6 @@ export const docs = defineDocs({
 export default defineConfig({
   plugins: [lastModified()],
   mdxOptions: {
-    remarkPlugins: [remarkVersions],
     remarkCodeTabOptions: {
       parseMdx: true,
     },
